7513239738e60d27d8a2b7f3cd347295cffdcdf6
cfiander <cfiander90@gmail.com>
2019-06-14 14:20:06 -0700

    done

---
+++ src/store.js
@@ -0,0 +1,18 @@
+const bookmarks = [
+    {
+        id: 1,
+        title: 'Google',
+        url: 'https://www.google.com',
+        description: 'A search engine',
+        rating: 4
+      },
+    {
+        id: 2,
+        title: 'Reddit',
+        url: 'https://www.reddit.com',
+        description: 'A forum website',
+        rating: 5
+      }
+]
+
+module.exports =  { bookmarks };
